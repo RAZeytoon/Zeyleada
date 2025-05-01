c586708b137b453c6204dc078be9a699b8ba83f9
Reza Yousefpour <R.A.Zeytoon@gmail.com>
2025-05-01 17:13:40 +0330

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -76,7 +76,7 @@ const config: QuartzConfig = {
     filters: [Plugin.RemoveDrafts()],
     emitters: [
       Plugin.AliasRedirects(),
-      Plugin.ComponentResources(),
+      //Plugin.ComponentResources(),
       Plugin.ContentPage(),
       Plugin.FolderPage(),
       Plugin.TagPage(),
